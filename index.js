acbf62a8c724f3ded4fb66d3153b7748c07d9b94
Kamil Alfaris <kamilalfaris@gmail.com>
2026-02-12 20:26:50 +0700

    update start script and dynamic port

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.post("/products", async (req, res) => {
   res.json(result.rows[0]);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
