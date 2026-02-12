7df8539a540252548fe3e288d7b19a7829a50098
Kamil Alfaris <kamilalfaris@gmail.com>
2026-02-12 20:49:21 +0700

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { pool } from "./db.js";
 
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
