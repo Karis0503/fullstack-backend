75b4b42044fe896dcfeba8237a77cf0181c65479
Kamil Alfaris <kamilalfaris@gmail.com>
2026-02-12 21:23:12 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const { pool } = require("./db");
+import express from "express";
+import cors from "cors";
+import { pool } from "./db.js";
 
 const app = express();
 app.use(cors());
