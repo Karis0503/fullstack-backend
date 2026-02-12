4717c58d8585216551e726b59b52953ed7a2e4d9
Kamil Alfaris <kamilalfaris@gmail.com>
2026-02-12 20:06:53 +0700

    initial backend

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import { pool } from "./db.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM products");
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  const { name } = req.body;
+  const result = await pool.query(
+    "INSERT INTO products(name) VALUES($1) RETURNING *",
+    [name]
+  );
+  res.json(result.rows[0]);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
